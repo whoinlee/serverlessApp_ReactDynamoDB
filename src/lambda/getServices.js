a12cd11d672e3c6cfd6ea680c91ea6fdab3e6ccc
Whoin Lee <whoinlee@gmail.com>
2023-06-11 12:39:26 -0400

    add lambda functions

---
+++ src/lambda/getServices.js
@@ -0,0 +1,23 @@
+//-- with node14.x & index.js
+
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-east-1",
+});
+
+var dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function (event, context, callback) {
+  var params = {
+    TableName: "Services",
+  };
+
+  dynamodb.scan(params, function (err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data.Items);
+    }
+  });
+};
